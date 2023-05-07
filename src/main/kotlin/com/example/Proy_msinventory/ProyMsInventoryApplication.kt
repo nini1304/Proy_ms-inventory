7b191f9ff73213e84ec480849784444e56f31a76
calc <geraldine.ibieta13@gmail.com>
2023-05-06 23:15:33 -0400

    Creacion del proyecto

---
+++ src/main/kotlin/com/example/Proy_msinventory/ProyMsInventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Proy_msinventory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyMsInventoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyMsInventoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Proy_msinventory/ProyMsInventoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Proy_msinventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyMsInventoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
